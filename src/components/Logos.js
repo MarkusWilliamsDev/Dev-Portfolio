cb290d8ba1f4783e42998280ef9482a781a1f322
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-05-15 13:54:38 -0400

    Map over logos

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -1,43 +1,34 @@
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { FaPython as Python } from "react-icons/fa";
+import {
+	GrGraphQl as GraphQL,
+	GrGatsbyjs as GatsbyJS,
+	GrNode as NodeJS,
+	GrReactjs as ReactJS,
+	GrJs as JS,
+} from "react-icons/gr";
+import { SiAdobecreativecloud as CreativeCloud } from "react-icons/si";
+
 export default function Example() {
+	const logos = [
+		{ icon: GraphQL, name: "GraphQL" },
+		{ icon: Python, name: "Python" },
+		{ icon: GatsbyJS, name: "GraphQL" },
+		{ icon: NodeJS, name: "NodeJS" },
+		{ icon: ReactJS, name: "ReactJS" },
+		{ icon: JS, name: "JavaScript" },
+		{ icon: CreativeCloud, name: "Adobe Creative Cloud" },
+	];
 	return (
 		<div className="bg-gray-50">
 			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-				<div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-							alt="Tuple"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-							alt="Mirage"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-							alt="StaticKit"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-							alt="Transistor"
-						/>
-					</div>
-					<div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-							alt="Workcation"
-						/>
-					</div>
+				<div className="flex justify-center h-full w-full">
+					{logos.map((logo) => (
+						<div className="w-full h-full">
+							<span className="h-full w-1/4">{<logo.icon />}</span>
+							<span>{logo.name}</span>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
