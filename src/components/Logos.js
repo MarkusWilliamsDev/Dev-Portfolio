842a3d8f9c51dbdb5b4bf56c118376f994b0c547
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-05-15 19:42:01 -0400

    Add some style to logos

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -4,9 +4,12 @@ import {
 	GrGatsbyjs as GatsbyJS,
 	GrNode as NodeJS,
 	GrReactjs as ReactJS,
-	GrJs as JS,
 } from "react-icons/gr";
-import { SiAdobecreativecloud as CreativeCloud } from "react-icons/si";
+import {
+	SiAdobecreativecloud as CreativeCloud,
+	SiJavascript as JS,
+	SiTailwindcss as Tailwind,
+} from "react-icons/si";
 
 export default function Example() {
 	const logos = [
@@ -16,16 +19,22 @@ export default function Example() {
 		{ icon: NodeJS, name: "NodeJS" },
 		{ icon: ReactJS, name: "ReactJS" },
 		{ icon: JS, name: "JavaScript" },
-		{ icon: CreativeCloud, name: "Adobe Creative Cloud" },
+		{ icon: CreativeCloud, name: "Adobe" },
+		{ icon: Tailwind, name: "TailwindCSS" },
 	];
 	return (
 		<div className="bg-gray-50">
 			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-				<div className="flex justify-center h-full w-full">
+				<div className="flex justify-center text-gray-900 font-mono font-semibold text-2xl p-4">
+					Skilled with
+				</div>
+				<div className="flex justify-center flex-wrap">
 					{logos.map((logo) => (
-						<div className="w-full h-full">
-							<span className="h-full w-1/4">{<logo.icon />}</span>
-							<span>{logo.name}</span>
+						<div className="w-xl min-w-xl h-full p-4 m-2 border-2 bg-gray-100 border-transparent rounded-lg drop-shadow-md hover:drop-shadow-lg cursor-default">
+							<span className="">{<logo.icon fill="rgb(79, 70, 229)" />}</span>
+							<span className="font-mono text-gray-700 text-lg ">
+								{logo.name}
+							</span>
 						</div>
 					))}
 				</div>
