6fd3a7ac9ceb0ec94f0dd2ec21b99e6b752a23d1
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-05-14 21:57:53 -0400

    Add boilerplate sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Hero from "./components/Hero";
+import CTA from "./components/Cta";
+import Logos from "./components/Logos";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<div>
+			<Hero />
+			<Logos />
+			<CTA />
+		</div>
+	);
 }
