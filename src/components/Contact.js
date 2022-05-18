075fcad14137c5040a32cf3ac85739b9b22ba68a
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-05-18 14:58:59 -0400

    Fix contact on mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,16 +24,16 @@ export default function Contact() {
 		},
 	];
 	return (
-		<div className="bg-gray-50 relative" id="contact">
+		<div className="flex bg-gray-50 relative" id="contact">
 			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-				<h3 className="flex justify-center relative text-gray-900 font-mono font-semibold text-2xl sm:text-4xl p-4">
+				<h3 className="text-center text-gray-900 font-mono font-semibold text-2xl sm:text-4xl p-4">
 					Contact Me
 				</h3>
-				<div className="flex justify-center space-x-8 h-32">
-					{/* TODO: Make responsive */}
+				<div className="flex flex-wrap justify-center p-2 lg:bg-indigo-200 lg:rounded-2xl">
+					{/* TODO: Resize & animate cards */}
 					{contactLogos.map((contactLogo) => (
 						<a href={contactLogo.href} target="_blank" rel="noreferrer">
-							<div className="p-6 m-2 sm:m-4 w-36 sm:w-60 bg-gray-100 rounded-lg drop-shadow-md hover:drop-shadow-lg">
+							<div className="p-6 mx-6 my-4 sm:m-4 w-36 sm:w-60 bg-gray-100 rounded-lg drop-shadow-md hover:drop-shadow-lg">
 								{
 									<contactLogo.icon className="w-full h-12 mb-2 text-indigo-600" />
 								}
