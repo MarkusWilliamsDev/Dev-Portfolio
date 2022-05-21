a55a4e609254d3732135ec03f4704ee14863d910
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-05-21 12:15:56 -0400

    Style projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,19 @@
 import React from "react";
 import gifGuesserLogo from "../assets/emoji.png";
-import react from "./logos/react.svg";
 import tailwind from "./logos/tailwindcss.svg";
 import firebase from "./logos/firebase.svg";
 import github from "./logos/github.svg";
 import externalLink from "./logos/externallink.svg";
+
+const reactLogo = (
+	<path d="M14.23 12.004a2.236 2.236 0 01-2.235 2.236 2.236 2.236 0 01-2.236-2.236 2.236 2.236 0 012.235-2.236 2.236 2.236 0 012.236 2.236zm2.648-10.69c-1.346 0-3.107.96-4.888 2.622-1.78-1.653-3.542-2.602-4.887-2.602-.41 0-.783.093-1.106.278-1.375.793-1.683 3.264-.973 6.365C1.98 8.917 0 10.42 0 12.004c0 1.59 1.99 3.097 5.043 4.03-.704 3.113-.39 5.588.988 6.38.32.187.69.275 1.102.275 1.345 0 3.107-.96 4.888-2.624 1.78 1.654 3.542 2.603 4.887 2.603.41 0 .783-.09 1.106-.275 1.374-.792 1.683-3.263.973-6.365C22.02 15.096 24 13.59 24 12.004c0-1.59-1.99-3.097-5.043-4.032.704-3.11.39-5.587-.988-6.38a2.167 2.167 0 00-1.092-.278zm-.005 1.09v.006c.225 0 .406.044.558.127.666.382.955 1.835.73 3.704-.054.46-.142.945-.25 1.44a23.476 23.476 0 00-3.107-.534A23.892 23.892 0 0012.769 4.7c1.592-1.48 3.087-2.292 4.105-2.295zm-9.77.02c1.012 0 2.514.808 4.11 2.28-.686.72-1.37 1.537-2.02 2.442a22.73 22.73 0 00-3.113.538 15.02 15.02 0 01-.254-1.42c-.23-1.868.054-3.32.714-3.707.19-.09.4-.127.563-.132zm4.882 3.05c.455.468.91.992 1.36 1.564-.44-.02-.89-.034-1.345-.034-.46 0-.915.01-1.36.034.44-.572.895-1.096 1.345-1.565zM12 8.1c.74 0 1.477.034 2.202.093.406.582.802 1.203 1.183 1.86.372.64.71 1.29 1.018 1.946-.308.655-.646 1.31-1.013 1.95-.38.66-.773 1.288-1.18 1.87a25.64 25.64 0 01-4.412.005 26.64 26.64 0 01-1.183-1.86c-.372-.64-.71-1.29-1.018-1.946a25.17 25.17 0 011.013-1.954c.38-.66.773-1.286 1.18-1.868A25.245 25.245 0 0112 8.098zm-3.635.254c-.24.377-.48.763-.704 1.16-.225.39-.435.782-.635 1.174-.265-.656-.49-1.31-.676-1.947.64-.15 1.315-.283 2.015-.386zm7.26 0c.695.103 1.365.23 2.006.387-.18.632-.405 1.282-.66 1.933a25.952 25.952 0 00-1.345-2.32zm3.063.675c.484.15.944.317 1.375.498 1.732.74 2.852 1.708 2.852 2.476-.005.768-1.125 1.74-2.857 2.475-.42.18-.88.342-1.355.493a23.966 23.966 0 00-1.1-2.98c.45-1.017.81-2.01 1.085-2.964zm-13.395.004c.278.96.645 1.957 1.1 2.98a23.142 23.142 0 00-1.086 2.964c-.484-.15-.944-.318-1.37-.5-1.732-.737-2.852-1.706-2.852-2.474 0-.768 1.12-1.742 2.852-2.476.42-.18.88-.342 1.356-.494zm11.678 4.28c.265.657.49 1.312.676 1.948-.64.157-1.316.29-2.016.39a25.819 25.819 0 001.341-2.338zm-9.945.02c.2.392.41.783.64 1.175.23.39.465.772.705 1.143a22.005 22.005 0 01-2.006-.386c.18-.63.406-1.282.66-1.933zM17.92 16.32c.112.493.2.968.254 1.423.23 1.868-.054 3.32-.714 3.708-.147.09-.338.128-.563.128-1.012 0-2.514-.807-4.11-2.28.686-.72 1.37-1.536 2.02-2.44 1.107-.118 2.154-.3 3.113-.54zm-11.83.01c.96.234 2.006.415 3.107.532.66.905 1.345 1.727 2.035 2.446-1.595 1.483-3.092 2.295-4.11 2.295a1.185 1.185 0 01-.553-.132c-.666-.38-.955-1.834-.73-3.703.054-.46.142-.944.25-1.438zm4.56.64c.44.02.89.034 1.345.034.46 0 .915-.01 1.36-.034-.44.572-.895 1.095-1.345 1.565-.455-.47-.91-.993-1.36-1.565z"></path>
+);
+const tailwindLogo = (
+	<path d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z" />
+);
+const firebaseLogo = (
+	<path d="M3.89 15.672L6.255.461A.542.542 0 017.27.288l2.543 4.771zm16.794 3.692l-2.25-14a.54.54 0 00-.919-.295L3.316 19.365l7.856 4.427a1.621 1.621 0 001.588 0zM14.3 7.147l-1.82-3.482a.542.542 0 00-.96 0L3.53 17.984z" />
+);
 
 const allProjects = [
 	{
@@ -15,9 +24,9 @@ const allProjects = [
 			"A web game built with React utilizing the Giphy API. You will be given a gif generated from a random word. Your job is to guess what the word is from just the gif.",
 		logo: gifGuesserLogo,
 		techUsed: [
-			{ name: "ReactJS", logo: react },
-			{ name: "TailwindCSS", logo: tailwind },
-			{ name: "Firebase", logo: firebase },
+			{ name: "ReactJS", logo: reactLogo },
+			{ name: "TailwindCSS", logo: tailwindLogo },
+			{ name: "Firebase", logo: firebaseLogo },
 		],
 	},
 	{
@@ -28,9 +37,9 @@ const allProjects = [
 			"A web game built with React utilizing the Giphy API. You will be given a gif generated from a random word. Your job is to guess what the word is from just the gif.",
 		logo: gifGuesserLogo,
 		techUsed: [
-			{ name: "ReactJS", logo: react },
-			{ name: "TailwindCSS", logo: tailwind },
-			{ name: "Firebase", logo: firebase },
+			{ name: "ReactJS", logo: reactLogo },
+			{ name: "TailwindCSS", logo: tailwindLogo },
+			{ name: "Firebase", logo: firebaseLogo },
 		],
 	},
 ];
@@ -61,29 +70,44 @@ export default function Projects() {
 									<div className="sm:col-span-2">
 										<div className="space-y-4">
 											<div className="text-lg leading-6 font-medium space-y-1">
-												<h3>{project.name}</h3>
+												<h3 className="text-2xl">{project.name}</h3>
 												<div className="flex space-x-4">
 													<a
 														href={project.github}
-														className="w-8 h-8"
 														target="_blank"
 														rel="noreferrer"
+														className="flex w-1/2 justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-indigo-700 text-slate-600 hover:text-indigo-600 fill-slate-600 hover:fill-indigo-600 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
 													>
-														<img src={github} alt="" />
+														<svg className="w-6 h-6">
+															<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
+														</svg>
+														<p className="font-mono">View code</p>
 													</a>
 													<a
 														href={project.link}
-														className="w-8 h-8"
 														target="_blank"
 														rel="noreferrer"
+														className="flex w-1/2 justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-indigo-700 text-slate-600 hover:text-indigo-600 fill-slate-600 hover:fill-indigo-600 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
 													>
-														<img src={externalLink} alt="" />
+														<p className="font-mono">Demo</p>
+														<svg className="w-6 h-6">
+															<path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+															<path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+														</svg>
 													</a>
 												</div>
 											</div>
 											<div className="text-lg">
 												<p className="text-gray-500">{project.about}</p>
 											</div>
+										</div>
+										<div className="flex text-gray-600 mt-4 h-8">
+											<p className="pr-2 text-lg">Powered by</p>
+											{project.techUsed.map((tech) => (
+												<svg className="w-6 fill-gray-500 mx-1">
+													{tech.logo}
+												</svg>
+											))}
 										</div>
 									</div>
 								</div>
