71d89a8bc314ac957eb1ef8387bb0bef3b4ef881
MarkusWilliams <markuswilliamsdev@gmail.com>
2023-03-24 17:28:09 -0400

    Updated hover colors and new skills

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,67 +1,44 @@
 import React from "react";
 import {
-	GrLinkedin as Linkedin,
-	GrMail as Mail,
-	GrGithub as Github,
-	GrChat as Text,
+  GrLinkedin as Linkedin,
+  GrMail as Mail,
+  GrGithub as Github,
 } from "react-icons/gr";
 
 export default function Contact() {
-	const contactLogos = [
-		{
-			icon: Linkedin,
-			name: "LinkedIn",
-			href: "https://www.linkedin.com/in/markus-williams-dev/",
-		},
-		{
-			icon: Mail,
-			name: "Email",
-			href: "mailto:markuswilliamsdev@gmail.com",
-		},
-		{
-			icon: Github,
-			name: "GitHub",
-			href: "https://github.com/MarkusWilliamsDev",
-		},
-		{
-			icon: Text,
-			name: "Text",
-			href: "sms:+17034631373",
-		},
-	];
+  const contactLogos = [
+    {
+      icon: Linkedin,
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/markus-williams-dev/",
+    },
+    {
+      icon: Mail,
+      name: "Email",
+      href: "mailto:markuswilliamsdev@gmail.com",
+    },
+    {
+      icon: Github,
+      name: "GitHub",
+      href: "https://github.com/MarkusWilliamsDev",
+    },
+  ];
 
-	const getMobileOS = () => {
-		const ua = navigator.userAgent;
-		if (/android/i.test(ua)) {
-			return "Android";
-		} else if (
-			/iPad|iPhone|iPod/.test(ua) ||
-			(navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
-		) {
-			return "iOS";
-		}
-		return "";
-	};
-
-	return (
-		<div className="flex bg-gray-50 relative" id="contact">
-			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-				<h3 className="text-center text-indigo-900 font-mono font-semibold text-2xl sm:text-4xl p-4">
-					Contact Me from your {getMobileOS()} device
-				</h3>
-
-				<div className="flex flex-wrap justify-center p-2">
-					{/* TODO: Resize & animate cards */}
-					{contactLogos.map((contactLogo) => (
-						<a href={contactLogo.href} target="_blank" rel="noreferrer">
-							<div className="p-6 mx-6 my-4 sm:m-4 w-36 sm:w-60 bg-gray-100 rounded-lg drop-shadow-md hover:drop-shadow-lg text-indigo-600 hover:text-indigo-800 transition-colors duration-200 ">
-								{<contactLogo.icon className="w-full h-12 mb-2 fill-current" />}
-								<p className="text-center font-medium">{contactLogo.name}</p>
-							</div>
-						</a>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex bg-gray-50 relative" id="contact">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap justify-center p-2">
+          {/* TODO: Resize & animate cards */}
+          {contactLogos.map((contactLogo) => (
+            <a href={contactLogo.href} target="_blank" rel="noreferrer">
+              <div className="p-6 mx-6 my-4 sm:m-4 w-36 sm:w-60 bg-gray-100 rounded-lg drop-shadow-md hover:drop-shadow-lg text-indigo-600 hover:text-purple-800 transition-colors duration-300 ">
+                {<contactLogo.icon className="w-full h-12 mb-2 fill-current" />}
+                <p className="text-center font-medium">{contactLogo.name}</p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Logos.js
+++ src/components/Logos.js
@@ -1,44 +1,43 @@
-import { FaPython as Python } from "react-icons/fa";
 import {
-	GrGraphQl as GraphQL,
-	GrGatsbyjs as GatsbyJS,
-	GrNode as NodeJS,
-	GrReactjs as ReactJS,
+  GrGraphQl as GraphQL,
+  GrNode as NodeJS,
+  GrReactjs as ReactJS,
 } from "react-icons/gr";
+import { SiJavascript as JS, SiTailwindcss as Tailwind } from "react-icons/si";
 import {
-	SiAdobecreativecloud as CreativeCloud,
-	SiJavascript as JS,
-	SiTailwindcss as Tailwind,
-} from "react-icons/si";
+  TbBrandNextjs as NextJS,
+  TbBrandTypescript as TS,
+} from "react-icons/tb";
+import SitecoreLogo from "./logos/SitecoreLogo";
 
 export default function Logos() {
-	const logos = [
-		{ icon: JS, name: "JavaScript" },
-		{ icon: NodeJS, name: "NodeJS" },
-		{ icon: ReactJS, name: "ReactJS" },
-		{ icon: GatsbyJS, name: "GatsbyJS" },
-		{ icon: GraphQL, name: "GraphQL" },
-		{ icon: Python, name: "Python" },
-		{ icon: Tailwind, name: "Tailwind" },
-		{ icon: CreativeCloud, name: "Adobe" },
-	];
-	return (
-		<div className="bg-gray-50 relative">
-			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-				<h1 className="flex justify-center text-indigo-900 font-mono font-semibold text-2xl sm:text-4xl p-4">
-					Skilled with
-				</h1>
-				<div className="flex justify-center flex-wrap text-indigo-800 hover:text-indigo-900 transition-color duration-300">
-					{logos.map((logo) => (
-						<div className="p-6 m-2 sm:m-4 w-36 sm:w-60 bg-gray-100 rounded-lg drop-shadow-md hover:drop-shadow-lg cursor-default">
-							{<logo.icon className="w-full h-12 mb-2 " />}
-							<span className="font-mono flex justify-center text-lg">
-								{logo.name}
-							</span>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const logos = [
+    { icon: JS, name: "JavaScript" },
+    { icon: TS, name: "TypeScript" },
+    { icon: NodeJS, name: "NodeJS" },
+    { icon: NextJS, name: "NextJS" },
+    { icon: ReactJS, name: "ReactJS" },
+    { icon: GraphQL, name: "GraphQL" },
+    { icon: Tailwind, name: "Tailwind" },
+    { icon: SitecoreLogo, name: "Sitecore" },
+  ];
+  return (
+    <div className="bg-gray-50 relative">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <h1 className="flex justify-center text-indigo-900 font-mono font-semibold text-2xl sm:text-4xl p-4">
+          Skilled with
+        </h1>
+        <div className="flex justify-center flex-wrap transition-color">
+          {logos.map((logo) => (
+            <div className="p-6 m-2 sm:m-4 w-36 sm:w-60 bg-gray-100 text-indigo-800  hover:text-purple-700 duration-300 rounded-lg drop-shadow-md hover:drop-shadow-lg cursor-default">
+              {<logo.icon className="w-full h-12 mb-22 " />}
+              <span className="font-mono flex justify-center text-lg">
+                {logo.name}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
